002837bac052c192a0a5b669f6d2cd360dc918b3
naveena0308 <naveena003office@gmail.com>
2025-10-09 12:05:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# churn_model/config.py
+RANDOM_STATE = 42
+TEST_SIZE = 0.3
+VAL_SIZE = 0.5
+MODEL_PATH = "models"
+DATA_PATH = "data/WA_Fn-UseC_-Telco-Customer-Churn.csv"
+EXPERIMENT_NAME = "churn_prediction"
